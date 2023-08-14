be4cba06b6dacfedacd823182e7087c60fff3e09
Manohar-1 <mudhapakamanohar@gmail.com>
2023-08-14 15:49:17 +0530

    User Resource CRUD Operations completed

---
+++ src/main/java/com/justreadit/blog/payload/APIResponse.java
@@ -0,0 +1,16 @@
+package com.justreadit.blog.payload;
+
+public class APIResponse {
+
+	public String message; 
+	public boolean status;
+	
+	public APIResponse(String message, boolean status) {
+		super();
+		this.message = message;
+		this.status = status;
+	} 
+	
+	
+	
+}
